6a5af70c3b0293ca2c5386a85901691dee9be057
aledolme <alessandra.dolmeta@vlsi22.vlsilab>
2023-02-27 11:24:11 +0100

    update repo

--- test/SHA3/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/gcc -o shake128 fips_202.c fips_202.h test_shake128.c -std=c99
-/usr/bin/gcc -o shake256 fips_202.c fips_202.h test_shake256.c -std=c99
-
--- test/keccak_ip_v2/build/keccak/kernel/irq_asm.d
+++
@@ -1,8 +0,0 @@
-/home/phd-students/alessandra.dolmeta/Desktop/pqc_riscv/test/keccak_ip_v2/build/keccak/kernel/irq_asm.o: \
- /home/phd-students/alessandra.dolmeta/Desktop/pqc_riscv/pulp-runtime/kernel/irq_asm.S \
- /home/phd-students/alessandra.dolmeta/Desktop/pqc_riscv/pulp-runtime/include/chips/pulpissimo/config.h \
- /home/phd-students/alessandra.dolmeta/Desktop/pqc_riscv/pulp-runtime/include/archi/pulp_defs.h
-
-/home/phd-students/alessandra.dolmeta/Desktop/pqc_riscv/pulp-runtime/include/chips/pulpissimo/config.h:
-
-/home/phd-students/alessandra.dolmeta/Desktop/pqc_riscv/pulp-runtime/include/archi/pulp_defs.h:
